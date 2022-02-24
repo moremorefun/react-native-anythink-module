a5bc35bd3e2bebd4d5b5fb983536b68db3de7abc
hao <unnamed.hao@gmail.com>
2022-02-24 18:46:18 +0800

    add log

--- android/src/main/java/com/reactnativeanythinkmodule/AnythinkBannerViewModule.java
+++ android/src/main/java/com/reactnativeanythinkmodule/AnythinkBannerViewModule.java
@@ -1,5 +1,6 @@
 package com.reactnativeanythinkmodule;
 
+import android.util.Log;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
@@ -10,6 +11,7 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
+import com.facebook.react.uimanager.annotations.ReactPropGroup;
 
 public class AnythinkBannerViewModule extends SimpleViewManager<FrameLayout> {
   public static final String REACT_CLASS = "AnythinkBannerView";
@@ -31,4 +33,30 @@ public class AnythinkBannerViewModule extends SimpleViewManager<FrameLayout> {
     return new FrameLayout(reactContext);
   }
 
+  @ReactProp(name = "placementID")
+  public void setPlacementID(FrameLayout view, @Nullable String placementID) {
+
+  }
+
+  @ReactProp(name = "width")
+  public void setWidth(FrameLayout view, float width) {
+
+  }
+
+  @ReactProp(name = "height")
+  public void setHeight(FrameLayout view, float height) {
+
+  }
+
+  @Override
+  public void onDropViewInstance(@NonNull FrameLayout view) {
+    super.onDropViewInstance(view);
+  }
+
+  @Override
+  protected void onAfterUpdateTransaction(@NonNull FrameLayout view) {
+    super.onAfterUpdateTransaction(view);
+    Log.i(REACT_CLASS, "onAfterUpdateTransaction width:" + view.getWidth() + " height: " + view.getHeight() );
+    Log.i(REACT_CLASS, "onAfterUpdateTransaction measuredWidth:" + view.getMeasuredWidth() + " measuredHeight: " + view.getMeasuredHeight() );
+  }
 }
