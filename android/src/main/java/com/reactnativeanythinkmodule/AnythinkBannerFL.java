f9c9340c6926b796a4aed58d60a4321520755b8d
hao <unnamed.hao@gmail.com>
2022-02-25 10:15:11 +0800

    fix size

--- android/src/main/java/com/reactnativeanythinkmodule/AnythinkBannerFL.java
+++ android/src/main/java/com/reactnativeanythinkmodule/AnythinkBannerFL.java
@@ -19,8 +19,8 @@ public class AnythinkBannerFL extends FrameLayout {
     super(context);
     mBannerView = new ATBannerView(mCallerContext.getCurrentActivity());
     mBannerView.setLayoutParams(new FrameLayout.LayoutParams(
-      300,
-      ViewGroup.LayoutParams.MATCH_PARENT
+      1200,
+      180
     ));
     mBannerView.setPlacementId("b62025d72e19ec");
     this.addView(mBannerView);
