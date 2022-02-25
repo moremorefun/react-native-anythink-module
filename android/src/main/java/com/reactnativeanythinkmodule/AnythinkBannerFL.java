d03347fe7204521be472e999b0f646eaccf64988
hao <unnamed.hao@gmail.com>
2022-02-25 16:27:28 +0800

    add event

--- android/src/main/java/com/reactnativeanythinkmodule/AnythinkBannerFL.java
+++ android/src/main/java/com/reactnativeanythinkmodule/AnythinkBannerFL.java
@@ -12,14 +12,20 @@ import com.anythink.banner.api.ATBannerListener;
 import com.anythink.banner.api.ATBannerView;
 import com.anythink.core.api.ATAdInfo;
 import com.anythink.core.api.AdError;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 
 public class AnythinkBannerFL extends FrameLayout {
   ReactApplicationContext mCallerContext;
+  ThemedReactContext mContext;
   ATBannerView mBannerView;
 
-  public AnythinkBannerFL( Context context, ReactApplicationContext mCallerContext) {
+  public AnythinkBannerFL( ThemedReactContext context, ReactApplicationContext mCallerContext) {
     super(context);
+    this.mContext = context;
     this.mCallerContext = mCallerContext;
   }
 
@@ -38,36 +44,84 @@ public class AnythinkBannerFL extends FrameLayout {
       @Override
       public void onBannerLoaded() {
         Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerLoaded");
+        WritableMap event = Arguments.createMap();
+        event.putString("type", "onBannerLoaded");
+        mContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+          AnythinkBannerFL.this.getId(),
+          "onEvent",
+          event);
       }
 
       @Override
       public void onBannerFailed(AdError adError) {
         Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerFailed:" + adError);
+        WritableMap event = Arguments.createMap();
+        event.putString("type", "onBannerFailed");
+        event.putString("adError", adError.toString());
+        mContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+          AnythinkBannerFL.this.getId(),
+          "onEvent",
+          event);
       }
 
       @Override
       public void onBannerClicked(ATAdInfo atAdInfo) {
         Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerClicked:" + atAdInfo);
+        WritableMap event = Arguments.createMap();
+        event.putString("type", "onBannerClicked");
+        event.putString("atAdInfo", atAdInfo.toString());
+        mContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+          AnythinkBannerFL.this.getId(),
+          "onEvent",
+          event);
       }
 
       @Override
       public void onBannerShow(ATAdInfo atAdInfo) {
         Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerShow:" + atAdInfo);
+        WritableMap event = Arguments.createMap();
+        event.putString("type", "onBannerShow");
+        event.putString("atAdInfo", atAdInfo.toString());
+        mContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+          AnythinkBannerFL.this.getId(),
+          "onEvent",
+          event);
       }
 
       @Override
       public void onBannerClose(ATAdInfo atAdInfo) {
         Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerClose:" + atAdInfo);
+        WritableMap event = Arguments.createMap();
+        event.putString("type", "onBannerClose");
+        event.putString("atAdInfo", atAdInfo.toString());
+        mContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+          AnythinkBannerFL.this.getId(),
+          "onEvent",
+          event);
       }
 
       @Override
       public void onBannerAutoRefreshed(ATAdInfo atAdInfo) {
         Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerAutoRefreshed:" + atAdInfo);
+        WritableMap event = Arguments.createMap();
+        event.putString("type", "onBannerAutoRefreshed");
+        event.putString("atAdInfo", atAdInfo.toString());
+        mContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+          AnythinkBannerFL.this.getId(),
+          "onEvent",
+          event);
       }
 
       @Override
       public void onBannerAutoRefreshFail(AdError adError) {
         Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerAutoRefreshFail:" + adError);
+        WritableMap event = Arguments.createMap();
+        event.putString("type", "onBannerAutoRefreshFail");
+        event.putString("adError", adError.toString());
+        mContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+          AnythinkBannerFL.this.getId(),
+          "onEvent",
+          event);
       }
     });
     this.addView(mBannerView);
--- android/src/main/java/com/reactnativeanythinkmodule/AnythinkBannerViewModule.java
+++ android/src/main/java/com/reactnativeanythinkmodule/AnythinkBannerViewModule.java
@@ -7,9 +7,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
+
+import java.util.Map;
 
 public class AnythinkBannerViewModule extends SimpleViewManager<AnythinkBannerFL> {
   public static final String REACT_CLASS = "AnythinkBannerView";
@@ -40,6 +43,14 @@ public class AnythinkBannerViewModule extends SimpleViewManager<AnythinkBannerFL
   public void onDropViewInstance(@NonNull AnythinkBannerFL view) {
     super.onDropViewInstance(view);
     view.destroy();
+  }
+
+  @Nullable
+  @Override
+  public Map<String, Object> getExportedCustomDirectEventTypeConstants() {
+    return MapBuilder.of(
+      "onEvent", MapBuilder.of("registrationName", "onEvent"));
+    // onNativeClick 是原生要发送的 event 名称，onReactClick 是 JS 端组件中注册的属性方法名称，中间的 registrationName 不可更改
   }
 
 }
