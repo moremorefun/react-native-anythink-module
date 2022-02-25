da752ca5eb8606444cfa47215a0587e111c83868
hao <unnamed.hao@gmail.com>
2022-02-25 09:31:50 +0800

    add log

--- android/src/main/java/com/reactnativeanythinkmodule/AnythinkBannerFL.java
+++ android/src/main/java/com/reactnativeanythinkmodule/AnythinkBannerFL.java
@@ -2,9 +2,14 @@ package com.reactnativeanythinkmodule;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.util.Log;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
+import com.anythink.banner.api.ATBannerListener;
 import com.anythink.banner.api.ATBannerView;
+import com.anythink.core.api.ATAdInfo;
+import com.anythink.core.api.AdError;
 import com.facebook.react.bridge.ReactApplicationContext;
 
 public class AnythinkBannerFL extends FrameLayout {
@@ -13,8 +18,50 @@ public class AnythinkBannerFL extends FrameLayout {
   public AnythinkBannerFL( Context context, ReactApplicationContext mCallerContext) {
     super(context);
     mBannerView = new ATBannerView(mCallerContext.getCurrentActivity());
+    mBannerView.setLayoutParams(new FrameLayout.LayoutParams(
+      ViewGroup.LayoutParams.WRAP_CONTENT,
+      ViewGroup.LayoutParams.WRAP_CONTENT
+    ));
     mBannerView.setPlacementId("b62025d72e19ec");
     this.addView(mBannerView);
+
+    mBannerView.setBannerAdListener(new ATBannerListener() {
+      @Override
+      public void onBannerLoaded() {
+        Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerLoaded");
+      }
+
+      @Override
+      public void onBannerFailed(AdError adError) {
+        Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerFailed:" + adError);
+      }
+
+      @Override
+      public void onBannerClicked(ATAdInfo atAdInfo) {
+        Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerClicked:" + atAdInfo);
+      }
+
+      @Override
+      public void onBannerShow(ATAdInfo atAdInfo) {
+        Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerShow:" + atAdInfo);
+      }
+
+      @Override
+      public void onBannerClose(ATAdInfo atAdInfo) {
+        Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerClose:" + atAdInfo);
+      }
+
+      @Override
+      public void onBannerAutoRefreshed(ATAdInfo atAdInfo) {
+        Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerAutoRefreshed:" + atAdInfo);
+      }
+
+      @Override
+      public void onBannerAutoRefreshFail(AdError adError) {
+        Log.i("AnythinkBannerView", "AnythinkBannerFL onBannerAutoRefreshFail:" + adError);
+      }
+    });
+
     mBannerView.loadAd();
   }
 
